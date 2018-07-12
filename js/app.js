989d8595ec23a1c42fce72570bff022001793be0
Dmitry <manxxeta@gmail.com>
2018-07-11 17:50:25 -0700

    Final for lab3

--- js/app.js
+++ js/app.js
@@ -2,135 +2,186 @@
 
 var wrongInputAlrt = 'Wrong input! Only y/n allowed';
 
+/*---------------------------------------------------------*/
+// amount of correct answers
+var correctAns = 0;
 
-// /*---------------------------------------------------------*/
-// // amount of correct answers
-// var correctAns = 0;
+/*---------------------------------------------------------*/
+// Question #3
+var guessActive = prompt('Do I like to be active? y/n');
 
-// /*---------------------------------------------------------*/
-// // Question #3
-// var guessActive = prompt('Do I like to be active? y/n');
+if (guessActive.toLowerCase() === 'n'){
 
-// if (guessActive.toLowerCase() === 'n'){
+    alert('You are wrong!');
 
-//     alert('You are wrong!');
+} else if (guessActive.toLowerCase() === 'y'){
 
-// } else if (guessActive.toLowerCase() === 'y'){
+    alert('Yes! I actually do!');
+    correctAns++;
 
-//     alert('Yes! I actually do!');
-//     correctAns++;
-
-// } else alert(wrongInputAlrt);
-
-
-
-// /*---------------------------------------------------------*/
-// // Question #3
-// var guessGames = prompt('Do I play a video games? y/n');
-
-// if (guessGames.toLowerCase() === 'n'){
-
-//     alert('That\'s right, I do like them but doesn\'t play that often!');
-//     correctAns++;
-
-// } else if (guessGames.toLowerCase() === 'y'){
-
-//     alert('You are wrong!');
-
-// }else alert('Wrong input! Only y/n allowed');
+} else alert(wrongInputAlrt);
 
 
 
-// /*---------------------------------------------------------*/
-// // Question #3
-// var guessColors = prompt('Do I like to change background colors? y/n');
-// if (guessColors.toLowerCase() === 'n'){
+/*---------------------------------------------------------*/
+// Question #3
+var guessGames = prompt('Do I play a video games? y/n');
 
-//     alert('You are wrong!');
+if (guessGames.toLowerCase() === 'n'){
 
-// } else if (guessColors.toLowerCase() === 'y'){
+    alert('That\'s right, I do like them but doesn\'t play that often!');
+    correctAns++;
 
-//     alert('Yes! I actually do!');
-//     correctAns++;
+} else if (guessGames.toLowerCase() === 'y'){
 
-// }else alert('Wrong input! Only y/n allowed');
+    alert('You are wrong!');
 
-
-
-// /*---------------------------------------------------------*/
-// // Question #3
-// var guessLang = prompt('Does english in my native language? y/n');
-
-// if (guessLang.toLowerCase() === 'n'){
-
-//     document.getElementsByTagName('h1')[0].textContent = 'DMITЯY';
-
-//     alert('You are right!');
-//     correctAns++;
-
-// } else if (guessLang.toLowerCase() === 'y'){
-
-//     alert('You are wrong!');
-
-// }else alert('Wrong input! Only y/n allowed');
+}else alert('Wrong input! Only y/n allowed');
 
 
 
-// /*---------------------------------------------------------*/
-// // Question #3
-// var guessAnimals = prompt('Do I like animals? y/n');
+/*---------------------------------------------------------*/
+// Question #3
+var guessColors = prompt('Do I like to change background colors? y/n');
+if (guessColors.toLowerCase() === 'n'){
 
-// if (guessAnimals.toLowerCase() === 'n'){
+    alert('You are wrong!');
 
-//     alert('You are wrong!');
+} else if (guessColors.toLowerCase() === 'y'){
 
-// } else if (guessAnimals.toLowerCase() === 'y'){
+    alert('Yes! I actually do!');
+    correctAns++;
 
-//     alert('Yes, I do!');
-//     correctAns++;
+}else alert('Wrong input! Only y/n allowed');
 
-// }else alert('Wrong input! Only y/n allowed');
+
+
+/*---------------------------------------------------------*/
+// Question #3
+var guessLang = prompt('Does english in my native language? y/n');
+
+if (guessLang.toLowerCase() === 'n'){
+
+    document.getElementsByTagName('h1')[0].textContent = 'DMITЯY';
+
+    alert('You are right!');
+    correctAns++;
+
+} else if (guessLang.toLowerCase() === 'y'){
+
+    alert('You are wrong!');
+
+}else alert('Wrong input! Only y/n allowed');
+
+
+
+/*---------------------------------------------------------*/
+// Question #3
+var guessAnimals = prompt('Do I like animals? y/n');
+
+if (guessAnimals.toLowerCase() === 'n'){
+
+    alert('You are wrong!');
+
+} else if (guessAnimals.toLowerCase() === 'y'){
+
+    alert('Yes, I do!');
+    correctAns++;
+
+}else alert('Wrong input! Only y/n allowed');
 
 
 
 /*---------------------------------------------------------*/
 // Question #6
 
-var correctNumber = 18;
+// number will be random from 0 to 30
+var correctNumber = Math.floor(Math.random() * 30);
 var attempts = 4;
 
 while (attempts !== 0){
 
-    var guessNumber = prompt('Let\'s get serious. Guess a number from 1 to 27:');
+    var guessNumber = prompt('Let\'s get serious. Guess a number from 1 to 50:');
 
-    if (parseInt(guessNumber) === correctNumber){
+    if (Number.isInteger(parseInt(guessNumber))){
+        if (parseInt(guessNumber) === correctNumber){
 
-        console.log("True!");
-        alert('Great job! You guess it with ' + (5 - attempts) + ' attempt(s)');
-        break;
-    } 
-    else if (parseInt(guessNumber) < correctNumber){
-
-        attempts--;
-        alert("Wrong...Go higher!" + attempts + " attempts left");
-        
-    } 
-    else if (parseInt(guessNumber) > correctNumber){
-
-        alert("Wrong...Go lower!" + attempts + " attempts left");
-        attempts--; 
+            console.log("True!");
+            alert('Great job! You guess it with ' + (5 - attempts) + ' attempt(s)');
+            correctAns++;
+            break;
+        } 
+        else if (parseInt(guessNumber) < correctNumber){
+    
+            console.log(parseInt(guessNumber) + ' lower than ' + correctNumber);
+            attempts--;
+            alert("Wrong...Go higher!" + attempts + " attempts left");
+            
+        } 
+        else if (parseInt(guessNumber) > correctNumber){
+    
+            console.log(parseInt(guessNumber) + ' bigger that ' + correctNumber);
+            attempts--; 
+            alert("Wrong...Go lower!" + attempts + " attempts left");
+        }
     }
+    else alert('Wrong input! It should be a whole number');
 }
 
 if ((attempts === 0) && (guessNumber !== correctNumber)){
 
     alert('You wasted all your attempts...');
-
 }
-
 
 /*---------------------------------------------------------*/
 // Question #7
+
+var citiesVisited = ['Moscow', 'Las Vegas', 'Rostov-On-Don', 'Vancouver', 'Seattle', 'Portland', 'Los Angeles'];
+var correctCityArr = [];
+
+for (var citiesAttempts = 5; citiesAttempts >= 0; citiesAttempts--)
+{
+    var cityGuess = prompt('Guess a city I\'ve been to: ');
+    var boolCorrect = false;
+
+    for (var k = 0; k < citiesVisited.length; k++){
+
+        if (cityGuess === citiesVisited[k]){
+
+            boolCorrect = true;
+            break;
+        }
+    }
+
+    if (boolCorrect){
+        correctCityArr.push(cityGuess);
+        alert('That\'s right! You have ' + citiesAttempts + ' attempts left');
+    }
+    else{
+        alert('That\'s wrong! You have ' + citiesAttempts + ' attempts left');
+    }
+}
+
+// checking which cities guess correctly (if any)
+if (correctCityArr.length !== 0){
+    alert('Done! Here is a cities you guessed: ' + correctCityArr);
+}
+else{
+    alert('Done! You didn\'t guessed any...');
+}
+
+// checking total score
+if (correctAns > 4){
+    alert('Your total score is: ' + correctAns + '/7. Not bad ;)');
+}
+else{
+    alert('Your total score is: ' + correctAns + '/7. You didn\'t try hard enough...');
+}
+
+
+
+
+
 
 /*---------------------------------------------------------*/
 // way to access element on page:
@@ -138,12 +189,10 @@ if ((attempts === 0) && (guessNumber !== correctNumber)){
 
 
 /*
-LAB#3: 
-use console.log()
+LAB#3 tasks: 
 use unordered list
 use ordered list
-do 6th question to guess in 4 attempts
-do 7th question to guess in 6 attempts using array
+
 
 
 */
